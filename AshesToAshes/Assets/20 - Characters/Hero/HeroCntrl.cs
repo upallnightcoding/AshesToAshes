a7eab5bb59aa21be33b12df158fbc3be9e774f05
upallnightcoding <upallnightcoding@gmail.com>
2025-05-06 01:11:31 -0400

    Started State Mechanics

--- AshesToAshes/Assets/20 - Characters/Hero/HeroCntrl.cs
+++ AshesToAshes/Assets/20 - Characters/Hero/HeroCntrl.cs
@@ -2,67 +2,108 @@ using A2A;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class HeroCntrl : MonoBehaviour
+namespace A2A
 {
-    [SerializeField] private float walkSpeed = 1.0f;
-    [SerializeField] private float runSpeed = 3.0f;
-    [SerializeField] private float animationBlendSpeed = 8.9f;
-    [SerializeField] private GameObject mainCamera;
-    [SerializeField] private float rotationSpeed = 20.0f;
-    [SerializeField] private Transform triggerPoint;
-    [SerializeField] private ProjectileSO projectileSO;
-    [SerializeField] private GameObject projectilePrefab;
-
-    private PlayerInputCntrl inputCntrl;
-    private Animator animator;
-
-    private int xVelocity;
-    private int yVelocity;
-
-    private Vector2 currentVelocity = Vector3.zero;
-
-    // Start is called before the first frame update
-    void Start()
+    public class HeroCntrl : MonoBehaviour
     {
-        animator = GetComponent<Animator>();
-        inputCntrl = GetComponent<PlayerInputCntrl>();
+        [SerializeField] private float walkSpeed = 1.0f;
+        [SerializeField] private float runSpeed = 3.0f;
+        [SerializeField] private float animationBlendSpeed = 8.9f;
+        [SerializeField] private GameObject mainCamera;
+        [SerializeField] private float rotationSpeed = 20.0f;
+        [SerializeField] private Transform triggerPoint;
+        [SerializeField] private ProjectileSO projectileSO;
+        [SerializeField] private GameObject projectilePrefab;
 
-        xVelocity = Animator.StringToHash("xVelocity");
-        yVelocity = Animator.StringToHash("yVelocity");
-    }
+        private PlayerInputCntrl inputCntrl;
+        private Animator animator;
 
-    // Update is called once per frame
-    void Update()
-    {
-        float targetRotation = mainCamera.transform.rotation.eulerAngles.y;
-        Quaternion rotation = Quaternion.Euler(0.0f, targetRotation, 0.0f);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+        private int xVelocityId;
+        private int yVelocityId;
+        private int fireId;
 
-        float moveSpeed = inputCntrl.Run ? runSpeed : walkSpeed;
-        currentVelocity.x = Mathf.Lerp(currentVelocity.x, inputCntrl.Move.x * moveSpeed, animationBlendSpeed * Time.deltaTime);
-        currentVelocity.y = Mathf.Lerp(currentVelocity.y, inputCntrl.Move.y * moveSpeed, animationBlendSpeed * Time.deltaTime);
+        private States currentState = States.MOVING;
 
-        animator.SetFloat(xVelocity, currentVelocity.x);
-        animator.SetFloat(yVelocity, currentVelocity.y);
+        private Vector2 currentVelocity = Vector3.zero;
 
-        if (inputCntrl.Fire)
+        // Start is called before the first frame update
+        void Start()
         {
-            //OnFire();
+            animator = GetComponent<Animator>();
+            inputCntrl = GetComponent<PlayerInputCntrl>();
+
+            xVelocityId = Animator.StringToHash("xVelocity");
+            yVelocityId = Animator.StringToHash("yVelocity");
+            fireId = Animator.StringToHash("Fire");
+        }
+
+        private void Update() 
+        {
+            switch(currentState)
+            {
+                case States.MOVING:
+                    currentState = State_Move();
+                    break;
+                case States.FIRE:
+                    currentState = State_Fire();
+                    break;
+            }    
+        }
+
+        private States State_Move()
+        {
+            States state = States.MOVING;
+
+            Move();
+
+            if (inputCntrl.Fire)
+            {
+                state = States.FIRE;
+            }
+
+            return (state);
+        }
+
+        private States State_Fire()
+        {
+            States state = States.MOVING;
+
+            OnAttack();
             inputCntrl.Fire = false;
+
+            return (state);
+        }
+
+        // Update is called once per frame
+        private void Move()
+        {
+            float targetRotation = mainCamera.transform.rotation.eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0.0f, targetRotation, 0.0f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+
+            float moveSpeed = inputCntrl.Run ? runSpeed : walkSpeed;
+            currentVelocity.x = Mathf.Lerp(currentVelocity.x, inputCntrl.Move.x * moveSpeed, animationBlendSpeed * Time.deltaTime);
+            currentVelocity.y = Mathf.Lerp(currentVelocity.y, inputCntrl.Move.y * moveSpeed, animationBlendSpeed * Time.deltaTime);
+
+            animator.SetFloat(xVelocityId, currentVelocity.x);
+            animator.SetFloat(yVelocityId, currentVelocity.y);
+        }
+
+        public void OnAttack()
+        {
+            Vector3 position = triggerPoint.position;
+            Vector3 direction = transform.forward;
+
+            GameObject go = Instantiate(projectilePrefab, position, Quaternion.identity);
+            go.GetComponent<Rigidbody>().AddForce(direction * projectileSO.speed, ForceMode.Impulse);
+            go.GetComponent<ProjectileCntrl>().SetProjectileSO(projectileSO).CreateProjectileTrail(go.transform, position);
+            Destroy(go, projectileSO.duration);
         }
     }
 
-    public void OnAttack()
+    public enum States
     {
-        Vector3 position = triggerPoint.position;
-        Vector3 direction = transform.forward;
-
-        GameObject go = Instantiate(projectilePrefab, position, Quaternion.identity);
-        go.GetComponent<Rigidbody>().AddForce(direction * projectileSO.speed, ForceMode.Impulse); 
-        go.GetComponent<ProjectileCntrl>().SetProjectileSO(projectileSO).CreateProjectileTrail(go.transform, position);
-        Destroy(go, projectileSO.duration);
+        MOVING,
+        FIRE
     }
 }
-
-
